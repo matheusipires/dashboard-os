d2a118379500c419ac78c1534f078fb2e16e3de9
matheusipires <matheuspires.eng@gmail.com>
2025-05-05 19:35:55 -0300

    leitura_dados.py

--- scripts/leitura_dados.py
+++ scripts/leitura_dados.py
@@ -1,12 +1,18 @@
 # scripts/leitura_dados.py
 
 import pandas as pd
-from config.paths import EXCEL_PATH, SHEET_NAME
 
 def carregar_dados():
-    """Lê o arquivo Excel e trata a coluna de data de abertura."""
-    df = pd.read_excel(EXCEL_PATH, sheet_name=SHEET_NAME)
+    """
+    Lê os dados de uma planilha pública do Google Sheets e trata a coluna de data de abertura.
+    """
+    url = "https://docs.google.com/spreadsheets/d/1Q2RfzoTasS-PDK6H1PC73p0uSalQQRlEyvV26rw2sKc/export?format=csv"
+    
+    df = pd.read_csv(url)
     df.columns = df.columns.str.strip()  # Remove espaços extras dos nomes das colunas
-    df['Abertura'] = pd.to_datetime(df['Abertura'], errors='coerce', dayfirst=True)
-    df = df.dropna(subset=['Abertura'])  # Remove linhas com data inválida
+
+    if 'Abertura' in df.columns:
+        df['Abertura'] = pd.to_datetime(df['Abertura'], errors='coerce', dayfirst=True)
+        df = df.dropna(subset=['Abertura'])  # Remove linhas com data inválida
+    
     return df
