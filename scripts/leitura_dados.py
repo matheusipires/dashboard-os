c3e888db7e3ff743426c3cec8e42fd08d15f668c
matheusipires <matheuspires.eng@gmail.com>
2025-05-05 19:44:35 -0300

    leitura_dados.py

--- scripts/leitura_dados.py
+++ scripts/leitura_dados.py
@@ -1,18 +1,17 @@
-# scripts/leitura_dados.py
-
 import pandas as pd
 
 def carregar_dados():
     """
-    Lê os dados de uma planilha pública do Google Sheets e trata a coluna de data de abertura.
+    Lê os dados da planilha pública do Google Sheets (.xlsx) e trata a coluna de data de abertura.
     """
-    url = "https://docs.google.com/spreadsheets/d/1Q2RfzoTasS-PDK6H1PC73p0uSalQQRlEyvV26rw2sKc/export?format=csv"
-    
-    df = pd.read_csv(url)
-    df.columns = df.columns.str.strip()  # Remove espaços extras dos nomes das colunas
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSjEallKbR3wVYefBHDbboAmYCktNQ2x9AfI-6tEISq99I1QrZZVmBUYzBIhbmdTATnUkUyqOXR0ZcR/pub?output=xlsx"
+
+    df = pd.read_excel(url)
+    df.columns = df.columns.str.strip()  # Remove espaços dos nomes de colunas
 
     if 'Abertura' in df.columns:
         df['Abertura'] = pd.to_datetime(df['Abertura'], errors='coerce', dayfirst=True)
-        df = df.dropna(subset=['Abertura'])  # Remove linhas com data inválida
-    
+        df = df.dropna(subset=['Abertura'])  # Remove linhas com datas inválidas
+
     return df
+
