e6b0a21561362cd0386e3fa63b1c0c9d3dfee4a7
Matheus Pires <matheuspires.eng@gmail.com>
2025-05-15 11:52:11 -0300

    leitura_dados.py

--- scripts/leitura_dados.py
+++ scripts/leitura_dados.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import streamlit as st
+
 @st.cache_data
 def carregar_dados():
     url = "https://drive.google.com/uc?export=download&id=1nwiU-O9DNjWGJ2C5PMp65uG2YBVoZnxM"
-    df = pd.read_excel(url, sheet_name="Dados sistemas fechadas")
     
-    # Garante que as datas estão no formato correto
+    # Carrega a planilha usando o nome correto da aba
+    df = pd.read_excel(url, sheet_name="Dados sistemas Fechadas")
+    
+    # Garante que as colunas de datas estão em datetime
     df['Abertura'] = pd.to_datetime(df['Abertura'], errors='coerce')
     df['Fechamento'] = pd.to_datetime(df['Fechamento'], errors='coerce')
     
